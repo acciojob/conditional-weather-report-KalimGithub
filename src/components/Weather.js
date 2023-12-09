2f7517cb83be5f5bced29c6bb21a3ce65d9f68af
KalimGithub <sk22kalim@gmail.com>
2023-12-10 01:41:13 +0530

    next

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,25 +6,17 @@ const Weather = () => {
     return (
         <div className='weather-container'>
             {(data.temperature > 20 && (
-                <div>
-                    <p style={{ color: 'red' }}>
-                        Current Temp : {data.temperature}
-                    </p>
-                    <p style={{ color: 'red' }}>
-                    Current Condition : {data.conditions}
-                    </p>
-                </div>
+                <p style={{ color: 'red' }}>
+                Temperature : {data.temperature} 
+                <span> Current Condition : {data.conditions}</span>
+                </p>
                 
             ))
             || (
-                <div>
                     <p style={{ color: 'blue' }}>
-                        Current Temp : {data.temperature}
+                    Temperature : {data.temperature}
+                    <span> Current Condition : {data.conditions}</span>
                     </p>
-                    <p style={{ color: 'blue' }}>
-                    Current Condition : {data.conditions}
-                    </p>
-                </div>
             )
             }
         </div>
